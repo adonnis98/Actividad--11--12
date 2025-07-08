2c126c4751f7e2cc564cc44fea551e5d619ddbe3
Admin <Admin@DESKTOP-ILL864I>
2025-07-08 00:04:55 -0500

    Agregar archivos de proyecto.

---
+++ Actividad--11--12/Clase/Beneficiario.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad__11__12.Clase
+{
+    public class Beneficiario
+    {
+        public int id;
+        public string idBeneficiario;
+        public string nombre;
+        public string apellido;
+        public string fechanacimineto;
+        public string telefono;
+        public string direccion;
+        private string historialmedico;
+        private string Informefundacion;
+
+        public Beneficiario(string idBeneficiario, string nombre, string apellido, string fechanacimineto, string telefono, 
+            string direccion, string historialmedico, string informefundacion)
+        {
+            this.idBeneficiario = idBeneficiario;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechanacimineto = fechanacimineto;
+            this.telefono = telefono;
+            this.direccion = direccion;
+            this.historialmedico = historialmedico;
+            this.Informefundacion = informefundacion;
+        }
+
+        public string GetHistorialMedico()
+        {
+            Console.WriteLine("Por favor, ingrese el código de acceso para ver el historial médico:");
+            string codigoIngresado = Console.ReadLine();
+
+            if (codigoIngresado == this.idBeneficiario)
+            {
+                return historialmedico;
+            }
+            else
+            {
+                Console.WriteLine("Acceso denegado, Código ingresado incorrecto. ");
+                return "codigo incorrecto";
+            }
+        }
+        public void SetHistorialMedico(string historialmedico)
+        {
+            this.historialmedico = historialmedico;
+        }
+
+
+        public string GetInformeFundacion()
+        {
+            Console.WriteLine("Por favor, ingrese el código de acceso para ver los informes del beneficiario: ");
+            string codigoIngresado = Console.ReadLine();
+
+            if (codigoIngresado == this.idBeneficiario)
+            {
+                return Informefundacion;
+            }
+            else
+            {
+                Console.WriteLine("Acceso denegado, Código ingresado incorrecto. ");
+                return "codigo incorrecto";
+            }
+        }
+
+        public void SetInformeFundacion(string informefundacion)
+        {
+            this.Informefundacion = informefundacion;
+        }
+     
+    }
+
+}
---
+++ Actividad--11--12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actividad__11__12.Clase;
+
+namespace Actividad__11__12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Beneficiario ObjBeneficiario = new Beneficiario("001", "Jeslyn", "Pluas", "2014, 03, 13", "0952037554", 
+                "Realidad de Dios Mz.6330 Sl.1", "Sin alergias conocidas", "Informe de fundación: Mejorar Comportamiento ");
+            Console.WriteLine("IdBeneficiario: " + ObjBeneficiario.idBeneficiario);
+            Console.WriteLine();
+            Console.WriteLine("Nombre: " + ObjBeneficiario.nombre);
+            Console.WriteLine();
+
+            Console.WriteLine("Apellido: " + ObjBeneficiario.apellido);
+            Console.WriteLine();
+
+            Console.WriteLine("Fecha de Nacimiento: " + ObjBeneficiario.fechanacimineto);
+            Console.WriteLine();
+
+            Console.WriteLine("Teléfono: " + ObjBeneficiario.telefono);
+            Console.WriteLine();
+
+            Console.WriteLine("Dirección: " + ObjBeneficiario.direccion);
+            Console.WriteLine();
+
+            Console.WriteLine("Historial Médico: " + ObjBeneficiario.GetHistorialMedico());
+            Console.WriteLine();
+
+            Console.WriteLine("Informe de Fundación: " + ObjBeneficiario.GetInformeFundacion());
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
